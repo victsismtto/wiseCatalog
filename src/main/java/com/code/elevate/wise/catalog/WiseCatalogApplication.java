841d1536411e2ceccbed88772dedd748eccef324
victor <victor.sismotto@gmail.com>
2025-08-09 14:57:00 -0300

    first commit

---
+++ src/main/java/com/code/elevate/wise/catalog/WiseCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.code.elevate.wise.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/elevate/wise/catalog/WiseCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.elevate.wise.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
