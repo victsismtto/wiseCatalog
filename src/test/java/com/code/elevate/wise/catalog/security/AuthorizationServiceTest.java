ccee17a024a4cdcdc0e964f07efc2062819bb81d
victor <victor.sismotto@gmail.com>
2025-08-12 00:40:31 -0300

    service authentication/authorization unit tests

---
+++ src/test/java/com/code/elevate/wise/catalog/security/AuthorizationServiceTest.java
@@ -0,0 +1,35 @@
+package com.code.elevate.wise.catalog.security;
+
+import com.code.elevate.wise.catalog.app.repository.UserRepository;
+import com.code.elevate.wise.catalog.app.service.security.AuthorizationService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuthorizationServiceTest {
+
+    @InjectMocks
+    private AuthorizationService service;
+
+    @Mock
+    private UserRepository repository;
+
+    @Test
+    void loadUserByUsername_shouldReturnUserDetails_whenUserExists() {
+        String username = "user";
+        UserDetails user = mock(UserDetails.class);
+        when(repository.findByLogin(username)).thenReturn(user);
+
+        UserDetails result = service.loadUserByUsername(username);
+
+        assertEquals(user, result);
+        verify(repository).findByLogin(username);
+    }
+}
